d6d3a2336e0ab668789c9d5328351d7294424537
ziyuan <ziyuan@2dfire.com>
2017-06-23 19:30:28 +0800

    constant pool

--- chapter3-cf/classfile/constant_pool.go
+++ chapter3-cf/classfile/constant_pool.go
@@ -9,18 +9,30 @@ func readConstantPool(reader *ClassReader) ConstantPool {
 	return nil
 }
 
+/**
+	获取常量信息
+ */
 func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
 	return nil
 }
 
+/**
+	获取名称和类型
+ */
 func (self ConstantPool) getNameAndType(index uint16) (string, string) {
 	return nil
 }
 
+/**
+	获取class name
+ */
 func (self ConstantPool) getClassName(index uint16) string {
 	return ""
 }
 
+/**
+	从常量池获取utf8编码的string
+ */
 func (self ConstantPool) getUtf8(index uint16) string {
 	return nil;
 }
