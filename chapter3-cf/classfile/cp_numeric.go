5090c549465cd28cdfcc17093fab93f7ae51f11c
ziyuan <ziyuan@2dfire.com>
2017-06-24 14:14:34 +0800

    constant nums

--- chapter3-cf/classfile/cp_numeric.go
+++ chapter3-cf/classfile/cp_numeric.go
@@ -1,29 +1,74 @@
 package chapter3_cf
 
+import "math"
+
 /**
 	常量池中integer
+	四字节存储整数常量
+	CONSTANT_INTEGER_INFO {
+		u1 tag;
+		u4 bytes;
+	}
  */
 type ConstantIntegerInfo struct {
+	//实际上，比int小的boolean、byte、short、char也可以放在里面
+	val int32
+}
 
+func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint32()
+	self.val = int32(bytes)
 }
 
 /**
 	常量池中float
+	四字节
+	CONSTANT_FLOAT_INFO {
+		u1 tag;
+		u4 bytes;
+	}
  */
 type ConstantFloatInfo struct {
+	val float32
+}
 
+func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint32()
+	self.val = math.Float32frombits(bytes)
 }
 
 /**
 	常量池中long
+	特殊一些 八字节，分成高8字节和低8字节
+	CONSTANT_LONG_INFO {
+		u1 tag;
+		u4 high_bytes;
+		u4 low_bytes;
+	}
  */
 type ConstantLongInfo struct {
+	val int64
+}
 
+func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+	self.val = int64(bytes)
 }
 
 /**
 	常量池中double
+	同样特殊 八字节
+	CONSTANT_DOUBLE_INFO {
+		u1 tag;
+		u4 high_bytes;
+		u4 low_bytes;
+	}
  */
 type ConstantDoubleInfo struct {
+	val float64
+}
 
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+	self.val = math.Float64frombits(bytes)
 }
