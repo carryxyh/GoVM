864368df3a874aa9cf8ba55454979991f63a5867
ziyuan <ziyuan@2dfire.com>
2017-06-29 19:52:22 +0800

    marker info

---
+++ chapter3-cf/classfile/attr_markers.go
@@ -0,0 +1,25 @@
+package chapter3_cf
+
+/**
+	用于支持@Deprecated注解
+ */
+type DeprecatedInfo struct {
+	MarkerAttribute
+}
+
+/**
+	用来标记源文件中不存在的、由编译器生成的类成员，主要为了支持嵌套类和嵌套接口
+ */
+type SyntheticInfo struct {
+	MarkerAttribute
+}
+
+/**
+	上面两个struct的父类，其中没有任何数据
+ */
+type MarkerAttribute struct {
+}
+
+func (self *MarkerAttribute) readInfo(reader *ClassReader) {
+	//read nothing
+}
