3edc929105d567e868e49ef814e751c634b94af4
ziyuan <ziyuan@2dfire.com>
2017-06-29 19:42:58 +0800

    unparsed info

--- chapter3-cf/classfile/attribute_info.go
+++ chapter3-cf/classfile/attribute_info.go
@@ -12,14 +12,51 @@ type AttributeInfo interface {
 	readInfo(reader *ClassReader)
 }
 
+/**
+	从常量池中解析出属性表中的属性
+ */
 func readAttributes(reader *ClassReader, cp ConstantPool) []AttributeInfo {
-	return nil
+	attributesCount := reader.readUint16()
+	attributes := make([]AttributeInfo, attributesCount)
+	for i := range attributes {
+		attributes[i] = readAttribute(reader, cp)
+	}
+	return attributes
 }
 
+/**
+	从常量池中解析一条属性表中的属性
+ */
 func readAttribute(reader *ClassReader, cp ConstantPool) AttributeInfo {
-	return nil
+	attributeNameIndex := reader.readUint16()
+	attrName := cp.getUtf8(attributeNameIndex)
+	attrLen := reader.readUint16()
+	attrInfo := newAttributeInfo(attrName, attrLen, cp)
+	attrInfo.readInfo(reader)
+	return attrInfo
 }
 
 func newAttributeInfo(attrName string, attrLen uint32, cp ConstantPool) AttributeInfo {
+	switch attrName {
+	case "Code":
+
+	case "ConstantValue":
+
+	case "Deprecated":
+
+	case "Exceptions":
+
+	case "LineNumberTable":
+
+	case "LocalVariableTable":
+
+	case "SourceFile":
+
+	case "Synthetic":
+
+	default:
+		return &UnparsedAttribute{attrName, attrLen, nil}
+
+	}
 	return nil
 }
