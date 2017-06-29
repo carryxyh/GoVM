9567cfb749e14b037bbe13ed770014c53441b92d
ziyuan <ziyuan@2dfire.com>
2017-06-29 20:02:48 +0800

    constant value attribute

---
+++ chapter3-cf/classfile/attr_constant_value.go
@@ -0,0 +1,21 @@
+package chapter3_cf
+
+/**
+	CONSTANTVALUE_ATTRIBUTE {
+		u2 attribute_name_index;
+		u4 attribute_length;
+		u2 constantvalue_index;
+	}
+ */
+type ConstantValueAttribute struct {
+	constantValueIndex uint16
+}
+
+func (self *ConstantValueAttribute) readInfo(reader *ClassReader) {
+	self.constantValueIndex = reader.readUint16()
+}
+
+func (self *ConstantValueAttribute) ConstantValueIndex() uint16 {
+	return self.constantValueIndex
+}
+
