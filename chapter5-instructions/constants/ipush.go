0ebee2428bf2ec83f88cd603510c68c177159bbc
ziyuan <ziyuan@2dfire.com>
2017-07-07 20:20:19 +0800

    bipush and sipush

---
+++ chapter5-instructions/constants/ipush.go
@@ -0,0 +1,38 @@
+package constants
+
+import (
+	"GoVM/chapter5-instructions/base"
+	"GoVM/chapter4-rtdt"
+)
+
+/**
+	从操作数中获取一个byte型证书，扩展成int型，然后推入栈顶
+ */
+type BIPUSH struct {
+	val int8
+}
+
+func (self *BIPUSH) FethchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt8()
+}
+
+func (self *BIPUSH) Execute(frame *chapter4_rtdt.Frame) {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
+}
+
+/**
+	从操作数中获取一个short型证书，扩展成int型，然后推入栈顶
+ */
+type SIPUSH struct {
+	val int16
+}
+
+func (self *SIPUSH) FethchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt16()
+}
+
+func (self *SIPUSH) Execute(frame *chapter4_rtdt.Frame) {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
+}
