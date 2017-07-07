f369fac5826688fe679da229b780b15c52f434b1
ziyuan <ziyuan@2dfire.com>
2017-07-07 19:53:37 +0800

    指令集和解释器

---
+++ chapter5-instructions/base/bytecode_reader.go
@@ -0,0 +1,44 @@
+package base
+
+type BytecodeReader struct {
+	//字节码
+	code []byte
+	//标记读取到了哪个字节
+	pc   int
+}
+
+/**
+	为了避免每次解码指令都新创建一个BytecodeReader实例
+ */
+func (self *BytecodeReader) Reset(code []byte, pc int) {
+	self.code = code
+	self.pc = pc
+}
+
+func (self *BytecodeReader) ReadUInt8() uint8 {
+	i := self.code[self.pc]
+	self.pc++
+	return i
+}
+
+func (self *BytecodeReader) ReadInt8() int8 {
+	return int8(self.ReadUInt8())
+}
+
+func (self *BytecodeReader) ReadUInt16() uint16 {
+	b1 := uint16(self.ReadUInt8())
+	b2 := uint16(self.ReadUInt8())
+	return (b1 << 8) | b2
+}
+
+func (self *BytecodeReader) ReadInt16() uint16 {
+	return int16(self.ReadUInt16())
+}
+
+func (self *BytecodeReader) ReadInt32() int32 {
+	b1 := int32(self.ReadUInt8())
+	b2 := int32(self.ReadUInt8())
+	b3 := int32(self.ReadUInt8())
+	b4 := int32(self.ReadUInt8())
+	return (b1 << 24) | (b2 << 16) | (b3 << 8) | b4
+}
---
+++ chapter5-instructions/base/instructions.go
@@ -0,0 +1,60 @@
+package base
+
+import "GoVM/chapter4-rtdt"
+
+type Instructions interface {
+	//从字节码中提取操作数
+	FetchOperands(reader *BytecodeReader)
+	//执行具体指令
+	Execute(frame *chapter4_rtdt.Frame)
+}
+
+type NoOperandsInstruction struct {
+
+}
+
+/**
+	什么也不做，这是个没有操作数的指令
+ */
+func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
+
+}
+
+/**
+	表示跳转指令
+ */
+type BranchInstruction struct {
+	Offset int
+}
+
+/**
+	读取一个16字节的int
+ */
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+	self.Offset = int(reader.ReadInt16())
+}
+
+/**
+	存储和加载指令需要根据索引存取 -> 局部变量表
+	索引由单字节操作数给出
+	用Index表示局部变量表索引
+ */
+type Index8Instruction struct {
+	Index uint
+}
+
+func (self *Index8Instruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint8())
+}
+
+/**
+	用于指令访问运行时常量池，Index8Instruction用于访问局部变量表
+	有一些指令需要访问运行时常量池，常量池索引由两个字节的操作数给出
+ */
+type Index16Instruction struct {
+	Index uint
+}
+
+func (self *Index16Instruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint16())
+}
