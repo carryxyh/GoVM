51ab0baa9b88604800ae5ce85123345c910e98e6
ziyuan <ziyuan@2dfire.com>
2017-07-07 21:40:57 +0800

    rem 指令

---
+++ chapter5-instructions/math/rem.go
@@ -0,0 +1,22 @@
+package math
+
+import "GoVM/chapter5-instructions/base"
+
+/**
+	取余指令
+ */
+type DREM struct {
+	base.NoOperandsInstruction
+}
+
+type FREM struct {
+	base.NoOperandsInstruction
+}
+
+type IREM struct {
+	base.NoOperandsInstruction
+}
+
+type LREM struct {
+	base.NoOperandsInstruction
+}
