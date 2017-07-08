999a891e31d0e02170273cc7c9534b9288f1b534
ziyuan <ziyuan@2dfire.com>
2017-07-08 14:15:14 +0800

    移位指令

---
+++ chapter5-instructions/math/sh.go
@@ -0,0 +1,88 @@
+package math
+
+import (
+	"GoVM/chapter5-instructions/base"
+	"GoVM/chapter4-rtdt"
+)
+
+/**
+	位移指令分为左移和右移
+	右移又可以分为算术右移（有符号右移） 和 逻辑右移（无符号右移）
+ */
+type ISHL struct {
+	base.NoOperandsInstruction
+}
+
+func (self *ISHL) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := v1 << s
+	stack.PushInt(result)
+}
+
+type ISHR struct {
+	base.NoOperandsInstruction
+}
+
+func (self *ISHR) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := v1 >> s
+	stack.PushInt(result)
+}
+
+type IUSHR struct {
+	base.NoOperandsInstruction
+}
+
+func (self *IUSHR) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := int32(uint32(v1) >> s)
+	stack.PushInt(result)
+}
+
+type LSHL struct {
+	base.NoOperandsInstruction
+}
+
+func (self *LSHL) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 << s
+	stack.PushLong(result)
+}
+
+type LSHR struct {
+	base.NoOperandsInstruction
+}
+
+func (self LSHR) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 >> s
+	stack.PushLong(result)
+}
+
+type IUSHR struct {
+	base.NoOperandsInstruction
+}
+
+func (self *LUSHR) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := int64(uint64(v1) >> s)
+	stack.PushLong(result)
+}
