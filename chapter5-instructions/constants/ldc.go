f77aa06383753f0d49bb1d9a26e677f5aa2fdb0d
ziyuan <ziyuan@2dfire.com>
2017-07-17 19:47:58 +0800

    ldc

---
+++ chapter5-instructions/constants/ldc.go
@@ -0,0 +1,69 @@
+package constants
+
+import (
+	"GoVM/chapter5-instructions/base"
+	"GoVM/chapter4-rtdt"
+	"GoVM/chapter6-obj/heap"
+)
+
+/**
+	用于从运行时常量池中加载常量值，并把它推入到操作数栈
+ */
+type LDC struct {
+	base.Index8Instruction
+}
+
+func (self *LDC) Execute(frame *chapter4_rtdt.Frame) {
+	_ldc(frame, self.Index)
+}
+
+/**
+	与ldc指令一样，都是加载int、float和字符串常量
+ */
+type LDC_W struct {
+	base.Index16Instruction
+}
+
+func (self *LDC_W) Execute(frame *chapter4_rtdt.Frame) {
+	_ldc(frame, self.Index)
+}
+
+/**
+	用于加载long、double常量
+ */
+type LDC2_W struct {
+	base.Index16Instruction
+}
+
+func (self *LDC2_W) Execute(frame *chapter4_rtdt.Frame) {
+	stack := frame.OperandStack()
+	cp := frame.Method().Class().ConstantPool()
+	c := cp.GetConstant(self.Index)
+
+	switch c.(type) {
+	case int64:
+		stack.PushLong(c.(int64))
+	case float64:
+		stack.PushDouble(c.(float64))
+	default:
+		panic("java.lang.ClassFormatError")
+	}
+}
+
+func _ldc(frame *chapter4_rtdt.Frame, index uint) {
+	stack := frame.OperandStack()
+	cp := frame.Method().Class().ConstantPool()
+	c := cp.GetConstant(index)
+
+	switch c.(type) {
+	case int32:
+		stack.PushInt(c.(int32))
+	case float32:
+		stack.PushFloat(c.(float32))
+	case string:
+	case heap.ClassRef:
+	default:
+		panic("todo: ldc")
+	}
+}
+
