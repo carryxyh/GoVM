d0184a08f14683a502c24654e3d4c28fea521fb5
ziyuan <ziyuan@2dfire.com>
2017-07-18 20:57:41 +0800

    非接口方法引用

--- chapter6-obj/heap/cp_methodref.go
+++ chapter6-obj/heap/cp_methodref.go
@@ -60,10 +60,45 @@ func (self *MethodRef) resolveMethodRef() {
 	self.method = method
 }
 
+/**
+	非接口方法引用的解析
+ */
 func lookupMethod(class *Class, name, descriptor string) *Method {
 	method := LookupMethodInClass(class, name, descriptor)
 	if method == nil {
 		method = lookupMethodInInterface(class.interfaces, name, descriptor)
 	}
 	return method
+}
+
+/**
+	从父类中找方法
+ */
+func LookupMethodInClass(class *Class, name, descriptor string) *Method {
+	for c := class; c != nil; c = c.superClass {
+		for _, method := range c.methods {
+			if method.name == name && method.descriptor == descriptor {
+				return method
+			}
+		}
+	}
+	return nil
+}
+
+/**
+	从接口中找方法
+ */
+func lookupMethodInInterface(ifaces []*Class, name, descriptor string) *Method {
+	for _, iface := range ifaces {
+		for _, method := range iface.methods {
+			if method.name == name && method.descriptor == descriptor {
+				return method
+			}
+		}
+		method := lookupMethodInInterface(iface.interfaces, name, descriptor)
+		if method != nil {
+			return method
+		}
+	}
+	return nil
 }
