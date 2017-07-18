0a98bc47dd40034957a31ce595bb7f06db241054
ziyuan <ziyuan@2dfire.com>
2017-07-18 21:19:34 +0800

    解析接口方法引用

--- chapter6-obj/heap/cp_interface_methodref.go
+++ chapter6-obj/heap/cp_interface_methodref.go
@@ -13,3 +13,37 @@ func newInterfaceMethodRef(cp *ConstantPool, interfaceMethodRef *chapter3_cf.Con
 	ref.copyMemberRefInfo(&interfaceMethodRef.ConstantMemberrefInfo)
 	return ref
 }
+
+func (self *InterfaceMethodRef) ResolvedInterfaceMethod() *Method {
+	if self.method == nil {
+		self.resolveInterfaceMethodRef()
+	}
+	return self.method
+}
+
+func (self *InterfaceMethodRef) resolveInterfaceMethodRef() {
+	d := self.cp.class
+	c := self.ResolvedClass()
+	if !c.IsInterface() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+
+	method := lookupInterfaceMethod(c, self.name, self.descriptor)
+	if method == nil {
+		panic("java.lang.NoSuchMethodError")
+	}
+	if !method.isAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+
+	self.method = method
+}
+
+func lookupInterfaceMethod(iface *Class, name, descriptor string) *Method {
+	for _, method := range iface.methods {
+		if method.name == name && method.descriptor == descriptor {
+			return method
+		}
+	}
+	return lookupMethodInInterface(iface.interfaces, name, descriptor)
+}
